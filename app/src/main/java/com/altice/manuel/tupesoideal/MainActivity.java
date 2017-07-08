86bfe8097d1b4a3b87a89e79a821c6e913ca55f1
many07 <manuelabud07@gmail.com>
2017-07-08 11:49:30 -0400

    Minimizar en OnCreate

--- app/src/main/java/com/altice/manuel/tupesoideal/MainActivity.java
+++ app/src/main/java/com/altice/manuel/tupesoideal/MainActivity.java
@@ -48,122 +48,30 @@ public class MainActivity extends AppCompatActivity{
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
         spinnerEstatura.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position==1){
-                    findViewById(R.id.estatura_pies_entry).setVisibility(View.VISIBLE);
-                    findViewById(R.id.estatura_pulgs_entry).setVisibility(View.VISIBLE);
-                    findViewById(R.id.estatura_mts_entry).setVisibility(View.GONE);
-                    estaturaEnPies=true;
-                }else{
-                    findViewById(R.id.estatura_pies_entry).setVisibility(View.GONE);
-                    findViewById(R.id.estatura_pulgs_entry).setVisibility(View.GONE);
-                    findViewById(R.id.estatura_mts_entry).setVisibility(View.VISIBLE);
-                    estaturaEnPies=false;
-                }
+                //Se esconden las vistas de libras o de kilogramos
+                esconderVistas(position);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
         botonResultados.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Se esconde el teclado una clickeado el botón
-                hideKeyboard(MainActivity.this);
-                resultados.setVisibility(View.GONE);
-                double estatura;
-                //Se toman los datos de estatura ingresados por el usuario
-                try {
-                    if(estaturaEnPies){
-                        EditText estaturaPiesEntry = (EditText) findViewById(R.id.estatura_pies_entry);
-                        EditText estaturaPulgsEntry = (EditText) findViewById(R.id.estatura_pulgs_entry);
-                        int pies = Integer.parseInt(estaturaPiesEntry.getText().toString());
-                        int pulgs = Integer.parseInt(estaturaPulgsEntry.getText().toString());
-
-                        estatura = (pies*12)+pulgs;
-                        estatura = estatura*0.0254;
-
-                    }else{
-                        EditText estaturaEntry = (EditText) findViewById(R.id.estatura_mts_entry);
-                        estatura = Double.parseDouble(estaturaEntry.getText().toString());
-                    }
-                }catch (Exception e){
-                    Toast.makeText(MainActivity.this, "Debes ingresar tu estatura",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-
-
-                //Se toman los datos de masa ingresados por el usuario
-                EditText masaEntry = (EditText) findViewById(R.id.masa_entry);
-                double masa;
-                try{
-                    masa = Double.parseDouble(masaEntry.getText().toString());
-                }catch (Exception ex){
-                    masa = 0;
-                }
-                if(masaEnLibras){
-                    masa=masa*0.4536;
-                }
-
-                //Se calcula el índice de masa corporal
-                double imc = masa/(estatura*estatura);
-                //Se escribe el resultado en el View correspondiente
-                TextView imcTextView = (TextView) findViewById(R.id.resultado_imc);
-                NumberFormat df = DecimalFormat.getInstance();
-                df.setMaximumFractionDigits(4);
-                imcTextView.setText(df.format(imc));
-                //Se asigna la condicion correspondiente
-                TextView condicionTextView = (TextView) findViewById(R.id.resultado_condicion);
-                TextView consejoTextView = (TextView) findViewById(R.id.resultado_consejo);
-                if(imc==0){
-                    condicionTextView.setText(R.string.condicion_indefinido);
-                    consejoTextView.setText(R.string.consejo_indefinido);
-                } else if(imc<18){
-                    condicionTextView.setText(R.string.condicion_bajo_peso);
-                    consejoTextView.setText(R.string.consejo_bajo_peso);
-                }else if(imc<25) {
-                    condicionTextView.setText(R.string.condicion_normal);
-                    consejoTextView.setText(R.string.consejo_normal);
-                }else if(imc<27){
-                    condicionTextView.setText(R.string.condicion_obesidad);
-                    consejoTextView.setText(R.string.consejo_obesidad);
-                }else if(imc<30){
-                    condicionTextView.setText(R.string.condicion_obesidad_grado_1);
-                    consejoTextView.setText(R.string.consejo_obesidad_grado_1);
-                }else if(imc<30){
-                    condicionTextView.setText(R.string.condicion_obesidad_grado_2);
-                    consejoTextView.setText(R.string.consejo_obesidad_grado_2);
-                }else if(imc<40){
-                    condicionTextView.setText(R.string.condicion_obesidad_grado_3);
-                    consejoTextView.setText(R.string.consejo_obesidad_grado_3);
-                }else{
-                    condicionTextView.setText(R.string.condicion_obesidad_extrema);
-                    consejoTextView.setText(R.string.consejo_obesidad_extrema);
-                }
-                resultados.setVisibility(View.VISIBLE);
-                double minimoRecomendado = 18*estatura*estatura;
-                double maximoRecomendado = 24.9*estatura*estatura;
-                String unidad = "kgs";
-                if(masaEnLibras){
-                    minimoRecomendado=minimoRecomendado*2.2;
-                    maximoRecomendado=maximoRecomendado*2.2;
-                    unidad = "lbs";
-                }
-                TextView pesoRecomendado = (TextView) findViewById(R.id.resultado_peso_recomendado);
-                pesoRecomendado.setText("Entre "+df.format(minimoRecomendado)+" "+unidad+" y "+df.format(maximoRecomendado)+" "+unidad);
+                presentarResultados();
             }
         });
     }
+
+    //Esta funcion sirve para esconder el teclado una vez presionado el boton
     public static void hideKeyboard(Activity activity) {
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         //Find the currently focused view, so we can grab the correct window token from it.
@@ -175,5 +83,113 @@ public class MainActivity extends AppCompatActivity{
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
+    //Esta funcion sirve para esconder los EditText no correspondientes
+    private void esconderVistas(int position){
+        if(position==1){
+            findViewById(R.id.estatura_pies_entry).setVisibility(View.VISIBLE);
+            findViewById(R.id.estatura_pulgs_entry).setVisibility(View.VISIBLE);
+            findViewById(R.id.estatura_mts_entry).setVisibility(View.GONE);
+            estaturaEnPies=true;
+        }else{
+            findViewById(R.id.estatura_pies_entry).setVisibility(View.GONE);
+            findViewById(R.id.estatura_pulgs_entry).setVisibility(View.GONE);
+            findViewById(R.id.estatura_mts_entry).setVisibility(View.VISIBLE);
+            estaturaEnPies=false;
+        }
+    }
 
+
+    private double calcularEstatura(){
+        double estatura;
+        if(estaturaEnPies){
+            EditText estaturaPiesEntry = (EditText) findViewById(R.id.estatura_pies_entry);
+            EditText estaturaPulgsEntry = (EditText) findViewById(R.id.estatura_pulgs_entry);
+            int pies = Integer.parseInt(estaturaPiesEntry.getText().toString());
+            int pulgs = Integer.parseInt(estaturaPulgsEntry.getText().toString());
+
+            estatura = (pies*12)+pulgs;
+            estatura = estatura*0.0254;
+
+        }else{
+            EditText estaturaEntry = (EditText) findViewById(R.id.estatura_mts_entry);
+            estatura = Double.parseDouble(estaturaEntry.getText().toString());
+        }
+        return estatura;
+    }
+
+    private void presentarResultados(){
+        //Se esconde el teclado una clickeado el botón
+        hideKeyboard(MainActivity.this);
+        resultados.setVisibility(View.GONE);
+        double estatura;
+        //Se toman los datos de estatura ingresados por el usuario
+
+        try {
+            estatura = calcularEstatura();
+        }catch (Exception e){
+            Toast.makeText(MainActivity.this, "Debes ingresar tu estatura",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+
+        //Se toman los datos de masa ingresados por el usuario
+        EditText masaEntry = (EditText) findViewById(R.id.masa_entry);
+        double masa;
+        try{
+            masa = Double.parseDouble(masaEntry.getText().toString());
+        }catch (Exception ex){
+            masa = 0;
+        }
+        if(masaEnLibras){
+            masa=masa*0.4536;
+        }
+
+        //Se calcula el índice de masa corporal
+        double imc = masa/(estatura*estatura);
+        //Se escribe el resultado en el View correspondiente
+        TextView imcTextView = (TextView) findViewById(R.id.resultado_imc);
+        NumberFormat df = DecimalFormat.getInstance();
+        df.setMaximumFractionDigits(4);
+        imcTextView.setText(df.format(imc));
+        //Se asigna la condicion correspondiente
+        TextView condicionTextView = (TextView) findViewById(R.id.resultado_condicion);
+        TextView consejoTextView = (TextView) findViewById(R.id.resultado_consejo);
+        if(imc==0){
+            condicionTextView.setText(R.string.condicion_indefinido);
+            consejoTextView.setText(R.string.consejo_indefinido);
+        } else if(imc<18){
+            condicionTextView.setText(R.string.condicion_bajo_peso);
+            consejoTextView.setText(R.string.consejo_bajo_peso);
+        }else if(imc<25) {
+            condicionTextView.setText(R.string.condicion_normal);
+            consejoTextView.setText(R.string.consejo_normal);
+        }else if(imc<27){
+            condicionTextView.setText(R.string.condicion_obesidad);
+            consejoTextView.setText(R.string.consejo_obesidad);
+        }else if(imc<30){
+            condicionTextView.setText(R.string.condicion_obesidad_grado_1);
+            consejoTextView.setText(R.string.consejo_obesidad_grado_1);
+        }else if(imc<30){
+            condicionTextView.setText(R.string.condicion_obesidad_grado_2);
+            consejoTextView.setText(R.string.consejo_obesidad_grado_2);
+        }else if(imc<40){
+            condicionTextView.setText(R.string.condicion_obesidad_grado_3);
+            consejoTextView.setText(R.string.consejo_obesidad_grado_3);
+        }else{
+            condicionTextView.setText(R.string.condicion_obesidad_extrema);
+            consejoTextView.setText(R.string.consejo_obesidad_extrema);
+        }
+        resultados.setVisibility(View.VISIBLE);
+        double minimoRecomendado = 18*estatura*estatura;
+        double maximoRecomendado = 24.9*estatura*estatura;
+        String unidad = "kgs";
+        if(masaEnLibras){
+            minimoRecomendado=minimoRecomendado*2.2;
+            maximoRecomendado=maximoRecomendado*2.2;
+            unidad = "lbs";
+        }
+        TextView pesoRecomendado = (TextView) findViewById(R.id.resultado_peso_recomendado);
+        pesoRecomendado.setText("Entre "+df.format(minimoRecomendado)+" "+unidad+" y "+df.format(maximoRecomendado)+" "+unidad);
+    }
 }
